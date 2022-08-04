677e9ab938be35b6877b8ba9ff12190c61f13173
Aleksandra <s.tsikhmanovich@gmail.com>
2022-08-04 12:23:07 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const baseLine = [];
+
+    for(let i =  0; i < expr.length; i += 10) {
+        const newStr = expr.substr (i, 10);
+        let subsrtChange = '';
+       
+
+        for(let j = 0; j < newStr.length; j += 2) {
+            const pair = newStr.substr(j, 2);
+
+            if( pair === '10') {
+                subsrtChange += '.'
+            } else if (pair === '11') {
+                subsrtChange += '-'
+            } else if (pair === '**') {
+                subsrtChange += '*'
+            }  
+        }
+
+        baseLine.push(subsrtChange);
+    }    
+    
+
+    const decodedLetters = baseLine.map(coded => MORSE_TABLE[coded] || ' ');
+    const resultLine = decodedLetters.join('');
+
+    return resultLine;
+}   
 
 module.exports = {
     decode
